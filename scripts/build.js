fc0b6cfe7a5ed743df5fb06864179c24b99be9c5
henry-wr <henrywahyu@gmail.com>
2020-05-15 14:28:34 +1000

    clean up temporary  properties folder and rebuild

--- scripts/build.js
+++ scripts/build.js
@@ -7,33 +7,23 @@ const styleDictionary = require('style-dictionary');
 const convertStringValues = obj =>
   Object.keys(obj).reduce((acc, key) => {
     const item = obj[key];
-    //check if item has a children key called 'value'
     const hasValueKey = !!item.value;
     const hasRgbKey = !!item.rgb;
 
     let resolvedValue;
 
-    //If item has children key called 'value' then pass the children object
-    console.log(item, hasRgbKey, hasValueKey);
-
     if (hasValueKey) {
       resolvedValue = item ;
-      //console.log('Object\nKey:' + key + '\nitem:' + item + '\nvalue:' + item.value);
     } 
 
-    //If item is just a string, set that string as the value
     else if (typeof item === 'string') {
-      resolvedValue = hasRgbKey? { rgb: item } : {value: item };
-      //console.log('Key-value pair\nkey:\t' + key + '\nvalue:\t' + item);
-    }
-    
-    //if item has children keys, run it through function again to drill down to the last branch
-    else {
-      resolvedValue = convertStringValues(item);
-      //console.log('Run through again:' + key + item + item.value);
+      resolvedValue = {value: item };
     }
 
-    //console.log(key, item.value, resolvedValue, item);
+    else {
+      resolvedValue = convertStringValues(item);
+    }
+
     return {
       ...acc,
       [key]: resolvedValue
@@ -75,5 +65,5 @@ const convertStringValues = obj =>
   sd.buildAllPlatforms();
 
   console.info('Removing build directory...');
-  //await fs.remove('.properties_json');
+  await fs.remove('.properties_json');
 })();
